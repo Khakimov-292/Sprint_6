e7ab32344ea35d63b30fb1fce3a62d5166ea0df5
Khakimov-292 <kyubi292@yandex.ru>
2025-04-10 15:26:54 +0300

    Delete locators directory

--- locators/base_page_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class BasePageLocators:
-    SCOOTER_BUTTON = [By.XPATH, ".//a[@href='/']"]
-    YANDEX_BUTTON = [By.XPATH, ".//a[@href='//yandex.ru']"]
