df7a903ac50b9c4bcc659b489a93f9dcf2871346
Khakimov-292 <kyubi292@yandex.ru>
2025-04-12 21:30:38 +0300

    Update main_page_locators.py

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     QUESTION = [By.XPATH, "(.//div[@class='accordion__button'])[{}]"]
     ANSWER = [By.XPATH, "(.//div[@class='accordion__panel'])[{}]"]
+    QUESTIONS = [By.CLASS_NAME, "accordion"]
