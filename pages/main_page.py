e67c5d9558024d037ca01863883c2a2250140aa9
Khakimov-292 <kyubi292@yandex.ru>
2025-04-12 21:29:39 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -13,13 +13,13 @@ class MainPage(BasePage):
     @allure.step("Извлечение вопроса")
     def get_question(self, index):
         question_locator = (MainPageLocators.QUESTION[0], MainPageLocators.QUESTION[1].format(index))
-        question = BasePage.wait_for_clickable(question_locator)
+        question = self.wait_for_clickable(question_locator)
         question.click()
         return question.text
 
     @allure.step("Извлечение ответа")
     def get_answers(self, index):
         answers_locator = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(index))
-        answers = BasePage.find_element(*answers_locator)
+        answers = self.find_element(*answers_locator)
         return answers.text
 
