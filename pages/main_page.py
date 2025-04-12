028e95694396f959897e0462093a81d0f2af5fa8
Khakimov-292 <kyubi292@yandex.ru>
2025-04-12 21:23:05 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,5 @@
 import allure
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from conftest import driver
+
 from locators.main_page_locators import MainPageLocators
 from pages.base_page import BasePage
 
@@ -10,20 +7,19 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
 
     @allure.step("Переход к вопросу")
-    def scroll_to_faq(self, driver):
-        element = driver.find_element(By.CLASS_NAME, "accordion")
-        driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    def scroll_to_faq(self):
+        self.go_to_element(MainPageLocators.QUESTION)
 
     @allure.step("Извлечение вопроса")
-    def get_question(self, driver, index):
+    def get_question(self, index):
         question_locator = (MainPageLocators.QUESTION[0], MainPageLocators.QUESTION[1].format(index))
-        question = WebDriverWait(driver, 3).until(ec.element_to_be_clickable(question_locator))
+        question = BasePage.wait_for_clickable(question_locator)
         question.click()
         return question.text
 
     @allure.step("Извлечение ответа")
-    def get_answers(self, driver, index):
+    def get_answers(self, index):
         answers_locator = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(index))
-        answers = driver.find_element(*answers_locator)
+        answers = BasePage.find_element(*answers_locator)
         return answers.text
 
