67a3e8289c62f91db862f9c52f61a5b7e1026a7b
Khakimov-292 <kyubi292@yandex.ru>
2025-04-12 21:22:10 +0300

    Update test_logo.py

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,22 +1,23 @@
 import allure
 from conftest import driver
+from data import Urls
 from pages.base_page import BasePage
 
 
 class TestURL:
     @allure.title('Проверка ссылки логотипа')
     def test_main_page(self, driver):
-        base_page = BasePage()
-        base_page.open_browser(driver)
-        base_page.click_order_button(driver)
-        base_page.click_scooter_button(driver)
-        base_page.should_main_page_url(driver)
+        base_page = BasePage(driver)
+        base_page.open_browser()
+        base_page.click_order_button()
+        base_page.click_scooter_button()
+        assert driver.current_url == Urls.MAIN_PAGE_URL
 
     @allure.title('Проверка ссылки "Яндекс"')
     def test_dzen_url(self, driver):
-        base_page = BasePage()
-        base_page.open_browser(driver)
-        base_page.click_dzen_button(driver)
-        base_page.switching_to_the_tab(driver)
-        base_page.wait_for_page_load(driver)
-        base_page.should_dzen_url(driver)
+        base_page = BasePage(driver)
+        base_page.open_browser()
+        base_page.click_dzen_button()
+        base_page.switching_to_the_tab()
+        base_page.wait_for_page_load()
+        assert driver.current_url == Urls.DZEN_URL
